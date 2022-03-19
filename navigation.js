cdccca6083efdfd140cefdb1c86f1789a4de638b
Marc LANTOL <marc.lantol@gmail.com>
2022-03-19 22:57:20 +0100

    add an event on burger links to hide the menu on click

--- navigation.js
+++ navigation.js
@@ -1,10 +1,14 @@
 function toogleNav() {
   const navbar = document.querySelector(".navbar");
   const burger = document.querySelector(".burger");
+  const navbarLinks = document.querySelector(".navbar__links");
   const bar = document.querySelector(".bar");
   burger.addEventListener("click", () => {
     navbar.classList.toggle("show-nav");
-    navbar.classList.toggle("hide-bar");
+    // bar.classList.toggle("bar__anim");
+    navbarLinks.addEventListener("click", () => {
+      navbar.classList.remove("show-nav");
+    });
   });
 }
 toogleNav();
