9d5e1eb51a5ccfd2b04c89e7d7473890c9543951
Marc LANTOL <marc.lantol@gmail.com>
2022-03-19 22:43:45 +0100

    fix toggle show-bar

--- navigation.js
+++ navigation.js
@@ -1,8 +1,10 @@
 function toogleNav() {
   const navbar = document.querySelector(".navbar");
   const burger = document.querySelector(".burger");
+  const bar = document.querySelector(".bar");
   burger.addEventListener("click", () => {
     navbar.classList.toggle("show-nav");
+    navbar.classList.toggle("hide-bar");
   });
 }
 toogleNav();
