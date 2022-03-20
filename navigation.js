249dfc010bb4a51ac0bfc76da54d78b8d50fe5ce
Marc LANTOL <marc.lantol@gmail.com>
2022-03-20 03:20:10 +0100

    try fixing menu anim

--- navigation.js
+++ navigation.js
@@ -1,14 +1,12 @@
-function toogleNav() {
-  const navbar = document.querySelector(".navbar");
-  const burger = document.querySelector(".burger");
-  const navbarLinks = document.querySelector("navbar__links");
-  const bar = document.querySelector(".bar");
-  burger.addEventListener("click", () => {
-    navbar.classList.toggle("show-nav");
-    // bar.classList.toggle("bar__anim");
-    navbarLinks.addEventListener("click", () => {
-      navbar.classList.remove("show-nav");
-    });
-  });
-}
-toogleNav();
+const navbar = document.querySelector(".navbar");
+const burger = document.querySelector(".burger");
+burger.addEventListener("click", () => {
+  navbar.classList.toggle("show-nav");
+});
+
+const navbarLinks = document.querySelector(".links");
+navbarLinks.addEventListener("click", () => {
+  navbar.classList.remove("show-nav");
+  navbar.classList.add("hide-nav");
+  navbar.classList.remove("hide-nav");
+});
