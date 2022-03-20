94e18b79ed7d97f5344be1980c0ee915d9a875c3
Marc LANTOL <marc.lantol@gmail.com>
2022-03-20 02:34:57 +0100

    fix menu and add links to footer

--- navigation.js
+++ navigation.js
@@ -1,7 +1,7 @@
 function toogleNav() {
   const navbar = document.querySelector(".navbar");
   const burger = document.querySelector(".burger");
-  const navbarLinks = document.querySelector(".navbar__links");
+  const navbarLinks = document.querySelector("navbar__links");
   const bar = document.querySelector(".bar");
   burger.addEventListener("click", () => {
     navbar.classList.toggle("show-nav");
