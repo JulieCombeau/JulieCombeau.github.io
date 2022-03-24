a5b7d3ed86172916f21fba9af3a5fe315f5d573a
Marc LANTOL <marc.lantol@gmail.com>
2022-03-24 10:18:16 +0100

    fix scrolling effect

---
+++ index.js
@@ -0,0 +1,57 @@
+const pres = document.querySelector(".presentation");
+const orga = document.querySelector(".organisation");
+const metho = document.querySelector(".methodology");
+const prob = document.querySelector(".problematics");
+const fun = document.querySelector(".fun");
+const foot = document.querySelector(".footer");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 500) {
+    pres.classList.add("scrollX");
+  } else {
+    pres.classList.remove("scrollX");
+  }
+});
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 1500) {
+    orga.classList.add("scrollX");
+  } else {
+    orga.classList.remove("scrollX");
+  }
+});
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 1500) {
+    metho.classList.add("scrollX");
+  } else {
+    metho.classList.remove("scrollX");
+  }
+});
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 1500) {
+    metho.classList.add("scrollX");
+  } else {
+    metho.classList.remove("scrollX");
+  }
+});
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 2500) {
+    prob.classList.add("scrollX");
+  } else {
+    prob.classList.remove("scrollX");
+  }
+});
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 2500) {
+    fun.classList.add("scrollX");
+  } else {
+    fun.classList.remove("scrollX");
+  }
+});
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 3000) {
+    foot.classList.add("scrollX");
+  } else {
+    foot.classList.remove("scrollX");
+  }
+});
