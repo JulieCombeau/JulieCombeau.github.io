7628de73c2b5008e2d3672b0389b9e9a1c438e9d
Marc LANTOL <marc.lantol@gmail.com>
2022-03-25 02:11:31 +0100

    fix desktop scrolling (reverse not working)

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-// const pres = document.querySelector(".presentation");
-// const orga = document.querySelector(".organisation");
-// const metho = document.querySelector(".methodology");
-// const prob = document.querySelector(".problematics");
-// const fun = document.querySelector(".fun");
-// const foot = document.querySelector(".footer");
+const pres = document.querySelector(".presentation");
+const orga = document.querySelector(".organisation");
+const metho = document.querySelector(".methodology");
+const prob = document.querySelector(".problematics");
+const fun = document.querySelector(".fun");
+const foot = document.querySelector(".footer");
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 200 && window.scrollY < 1500) {
+  if (
+    window.screen.width < 768 &&
+    window.scrollY > 100 &&
+    window.scrollY < 1500
+  ) {
     pres.classList.add("scrollX");
   } else {
     pres.classList.remove("scrollX");
@@ -14,35 +18,51 @@ window.addEventListener("scroll", () => {
 });
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 1200 && window.scrollY < 2500) {
+  if (
+    window.screen.width < 768 &&
+    window.scrollY > 1000 &&
+    window.scrollY < 2500
+  ) {
     orga.classList.add("scrollX");
   } else {
     orga.classList.remove("scrollX");
   }
 });
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 1800 && window.scrollY < 3000) {
+  if (
+    window.screen.width < 768 &&
+    window.scrollY > 1800 &&
+    window.scrollY < 3000
+  ) {
     metho.classList.add("scrollX");
   } else {
     metho.classList.remove("scrollX");
   }
 });
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 2300 && window.scrollY < 3500) {
+  if (
+    window.screen.width < 768 &&
+    window.scrollY > 2300 &&
+    window.scrollY < 3500
+  ) {
     prob.classList.add("scrollX");
   } else {
     prob.classList.remove("scrollX");
   }
 });
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 3200 && window.scrollY < 4500) {
+  if (
+    window.screen.width < 768 &&
+    window.scrollY > 3000 &&
+    window.scrollY < 4500
+  ) {
     fun.classList.add("scrollX");
   } else {
     fun.classList.remove("scrollX");
   }
 });
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 4000) {
+  if (window.screen.width < 768 && window.scrollY > 4000) {
     foot.classList.add("scrollX");
   } else {
     foot.classList.remove("scrollX");
@@ -52,47 +72,65 @@ window.addEventListener("scroll", () => {
 // ------------------------------------------------------------
 
 window.addEventListener("scroll", () => {
-  if (window.scrollY > 200 && window.scrollY < 1500) {
+  if (
+    window.screen.width > 769 &&
+    window.scrollY > 200 &&
+    window.scrollY < 3000
+  ) {
     pres.classList.add("presentation-desk");
   } else {
     pres.classList.remove("presentation-desk");
   }
 });
 
-console.log(scrollY);
-
-// window.addEventListener("scroll", () => {
-//   if (window.scrollY > 1300 && window.scrollY < 2500) {
-//     orga.classList.add("organisation-desk");
-//   } else {
-//     orga.classList.remove("organisation-desk");
-//   }
-// });
-// window.addEventListener("scroll", () => {
-//   if (window.scrollY > 1300 && window.scrollY < 2500) {
-//     metho.classList.add("methodology-desk");
-//   } else {
-//     metho.classList.remove("methodology-desk");
-//   }
-// });
-// window.addEventListener("scroll", () => {
-//   if (window.scrollY > 1800 && window.scrollY < 3500) {
-//     prob.classList.add("problematics-desk");
-//   } else {
-//     prob.classList.remove("problematics-desk");
-//   }
-// });
-// window.addEventListener("scroll", () => {
-//   if (window.scrollY > 1800 && window.scrollY < 3500) {
-//     fun.classList.add("fun-desk");
-//   } else {
-//     fun.classList.remove("fun-desk");
-//   }
-// });
-// window.addEventListener("scroll", () => {
-//   if (window.scrollY > 4000) {
-//     foot.classList.add("footer-desk");
-//   } else {
-//     foot.classList.remove("footer-desk");
-//   }
-// });
+window.addEventListener("scroll", () => {
+  if (
+    window.screen.width > 769 &&
+    window.scrollY > 1000 &&
+    window.scrollY < 7500
+  ) {
+    orga.classList.add("organisation-desk");
+  } else {
+    orga.classList.remove("organisation-desk");
+  }
+});
+window.addEventListener("scroll", () => {
+  if (
+    window.screen.width > 769 &&
+    window.scrollY > 1000 &&
+    window.scrollY < 7500
+  ) {
+    metho.classList.add("methodology-desk");
+  } else {
+    metho.classList.remove("methodology-desk");
+  }
+});
+window.addEventListener("scroll", () => {
+  if (
+    window.screen.width > 769 &&
+    window.scrollY > 1800 &&
+    window.scrollY < 10000
+  ) {
+    prob.classList.add("problematics-desk");
+  } else {
+    prob.classList.remove("problematics-desk");
+  }
+});
+window.addEventListener("scroll", () => {
+  if (
+    window.screen.width > 769 &&
+    window.scrollY > 1800 &&
+    window.scrollY < 10000
+  ) {
+    fun.classList.add("fun-desk");
+  } else {
+    fun.classList.remove("fun-desk");
+  }
+});
+window.addEventListener("scroll", () => {
+  if (window.screen.width > 769 && window.scrollY > 2000) {
+    foot.classList.add("footer-desk");
+  } else {
+    foot.classList.remove("footer-desk");
+  }
+});
